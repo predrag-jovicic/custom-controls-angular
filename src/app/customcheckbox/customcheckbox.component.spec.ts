e34164ce1e9241138992e30b907fa4d46f7b42cd
Predrag Jovicic <predragjovicic33@hotmail.com>
2021-04-02 01:57:16 +0200

    Migrate to angular v11

--- src/app/customcheckbox/customcheckbox.component.spec.ts
+++ src/app/customcheckbox/customcheckbox.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CustomcheckboxComponent } from './customcheckbox.component';
 
@@ -6,7 +6,7 @@ describe('CustomcheckboxComponent', () => {
   let component: CustomcheckboxComponent;
   let fixture: ComponentFixture<CustomcheckboxComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CustomcheckboxComponent ]
     })
--- src/app/customdropdownlist/customdropdownlist.component.spec.ts
+++ src/app/customdropdownlist/customdropdownlist.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CustomdropdownlistComponent } from './customdropdownlist.component';
 
@@ -6,7 +6,7 @@ describe('CustomdropdownlistComponent', () => {
   let component: CustomdropdownlistComponent;
   let fixture: ComponentFixture<CustomdropdownlistComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CustomdropdownlistComponent ]
     })
